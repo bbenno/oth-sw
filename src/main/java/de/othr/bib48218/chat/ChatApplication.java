9a55d850e381e36fed904a7cb24b223d8991f52c
Benno Bielmeier <benno.bielmeier@st.oth-regensburg.de>
2020-11-03 18:03:10 +0100

    initial commit
    This project was initialized by the spring initializr[1].
    
    [1]: https://start.spring.io

---
+++ src/main/java/de/othr/bib48218/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package de.othr.bib48218.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/othr/bib48218/chat/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package de.othr.bib48218.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
