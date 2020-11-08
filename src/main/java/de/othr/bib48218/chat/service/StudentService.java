839e34a27caa6f5906d0883d6df9f4a9cd68e7a3
Benno Bielmeier <benno.bielmeier@st.oth-regensburg.de>
2020-11-09 00:55:10 +0100

    implement dummy 'registerStudent' in service class

--- src/main/java/de/othr/bib48218/chat/service/StudentService.java
+++ src/main/java/de/othr/bib48218/chat/service/StudentService.java
@@ -1,9 +1,9 @@
 package de.othr.bib48218.chat.service;
 
+import de.othr.bib48218.chat.entity.Student;
 import de.othr.bib48218.chat.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,9 +12,13 @@ public class StudentService {
     @Autowired
     private StudentRepository studentRepository;
 
-    // TODO
-    @GetMapping("/hello")
-    public String helloWorld(@RequestParam(value = "name", defaultValue = "world") String input) {
-        return "Hello, " + input + "!";
+    @GetMapping("/students")
+    public Student registerStudent() {
+        // TODO: use provided student instead of created dummy
+        Student student = new Student(1234L, "Benno", "Bielmeier", "IN");
+
+        student = studentRepository.save(student);
+
+        return student;
     }
 }
