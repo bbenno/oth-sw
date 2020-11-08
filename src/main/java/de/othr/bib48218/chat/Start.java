0c26eade27d3ac97bba1031715b8d6ce11b9b6de
Benno Bielmeier <benno.bielmeier@st.oth-regensburg.de>
2020-11-09 00:27:59 +0100

    move role 'RestController' to service class

--- src/main/java/de/othr/bib48218/chat/Start.java
+++ src/main/java/de/othr/bib48218/chat/Start.java
@@ -2,20 +2,12 @@ package de.othr.bib48218.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Start {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Start.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String helloWorld(@RequestParam(value = "name", defaultValue = "world") String input) {
-		return "Hello, " + input + "!";
-	}
 }
--- src/main/java/de/othr/bib48218/chat/service/StudentService.java
+++ src/main/java/de/othr/bib48218/chat/service/StudentService.java
@@ -1,4 +1,15 @@
 package de.othr.bib48218.chat.service;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StudentService {
+
+    // TODO
+    @GetMapping("/hello")
+    public String helloWorld(@RequestParam(value = "name", defaultValue = "world") String input) {
+        return "Hello, " + input + "!";
+    }
 }
