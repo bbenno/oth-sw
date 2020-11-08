da6355fd20f2c69d06994eff39561aa9b3d895f9
Benno Bielmeier <benno.bielmeier@st.oth-regensburg.de>
2020-11-08 22:53:16 +0100

    make application REST controller

--- src/main/java/de/othr/bib48218/chat/ChatApplication.java
+++ src/main/java/de/othr/bib48218/chat/ChatApplication.java
@@ -2,8 +2,10 @@ package de.othr.bib48218.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ChatApplication {
 
 	public static void main(String[] args) {
