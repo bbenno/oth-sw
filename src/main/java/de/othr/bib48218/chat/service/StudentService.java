179e7f90cc98b3d84ab97cb9358d45d96adb86cf
Benno Bielmeier <benno.bielmeier@st.oth-regensburg.de>
2020-11-09 00:40:36 +0100

    add autowire of 'StudentRepository' to 'StudentService'

--- src/main/java/de/othr/bib48218/chat/service/StudentService.java
+++ src/main/java/de/othr/bib48218/chat/service/StudentService.java
@@ -1,11 +1,16 @@
 package de.othr.bib48218.chat.service;
 
+import de.othr.bib48218.chat.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
 
     // TODO
     @GetMapping("/hello")
