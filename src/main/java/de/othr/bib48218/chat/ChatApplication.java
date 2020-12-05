325f1278ed256e43024f9fa0a3f9cc8841be9544
Benno Bielmeier <github@bbenno.com>
2020-12-05 13:44:32 +0100

    Add logger to application's main

--- src/main/java/de/othr/bib48218/chat/ChatApplication.java
+++ src/main/java/de/othr/bib48218/chat/ChatApplication.java
@@ -1,10 +1,13 @@
 package de.othr.bib48218.chat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChatApplication {
+	private static final Logger log = LoggerFactory.getLogger(ChatApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatApplication.class, args);
