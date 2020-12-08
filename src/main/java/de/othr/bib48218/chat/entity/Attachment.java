26846a0096cca7d99352f6d96bbef66e206af265
Benno Bielmeier <github@bbenno.com>
2020-12-08 09:50:46 +0100

    Add constructor and getter

--- src/main/java/de/othr/bib48218/chat/entity/Attachment.java
+++ src/main/java/de/othr/bib48218/chat/entity/Attachment.java
@@ -7,4 +7,22 @@ public class Attachment extends IdEntity {
     private int size;
     private String name;
     private String mimeType;
+
+    protected Attachment() {}
+
+    public Attachment(int size, String name, String mimeType) {
+        this.size = size;
+        this.name = name;
+        this.mimeType = mimeType;
+    }
+
+    public int getSize() {
+        return size;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getMimeType() {
+        return mimeType;
+    }
 }
