0b6065fd3f37df7d420a6c0e59dd85dd2660ed78
Benno Bielmeier <github@bbenno.com>
2020-12-08 16:13:43 +0100

    Fix missing import

--- src/main/java/de/othr/bib48218/chat/controller/LoginController.java
+++ src/main/java/de/othr/bib48218/chat/controller/LoginController.java
@@ -2,6 +2,7 @@ package de.othr.bib48218.chat.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import de.othr.bib48218.chat.service.UserService;
