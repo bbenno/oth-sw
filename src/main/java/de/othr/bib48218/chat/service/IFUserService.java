9ad834559e271377f33583142dbb8cda6443400b
Benno Bielmeier <github@bbenno.com>
2020-12-18 10:24:29 +0100

    Add findByUsername to UserService

--- src/main/java/de/othr/bib48218/chat/service/IFUserService.java
+++ src/main/java/de/othr/bib48218/chat/service/IFUserService.java
@@ -1,4 +1,9 @@
 package de.othr.bib48218.chat.service;
 
+import de.othr.bib48218.chat.entity.User;
+
+import java.util.Optional;
+
 public interface IFUserService {
+    Optional<User> findByUsername(String username);
 }
--- src/main/java/de/othr/bib48218/chat/service/UserService.java
+++ src/main/java/de/othr/bib48218/chat/service/UserService.java
@@ -1,13 +1,21 @@
 package de.othr.bib48218.chat.service;
 
+import de.othr.bib48218.chat.entity.User;
 import de.othr.bib48218.chat.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService implements IFUserService {
     @Qualifier("personRepository")
     @Autowired
     private UserRepository repository;
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return repository.findById(username);
+    }
 }
