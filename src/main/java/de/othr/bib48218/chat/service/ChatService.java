dfff7ec79b43151ac55f0377aab982e1290f977c
Benno Bielmeier <github@bbenno.com>
2020-12-18 10:48:30 +0100

    Add getChatBetweenUsers to ChatService

--- src/main/java/de/othr/bib48218/chat/service/ChatService.java
+++ src/main/java/de/othr/bib48218/chat/service/ChatService.java
@@ -1,9 +1,13 @@
 package de.othr.bib48218.chat.service;
 
+import de.othr.bib48218.chat.entity.PeerChat;
+import de.othr.bib48218.chat.entity.User;
 import de.othr.bib48218.chat.repository.ChatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ChatService implements IFChatService {
@@ -14,4 +18,9 @@ public class ChatService implements IFChatService {
     @Qualifier("peerChatRepository")
     @Autowired
     private ChatRepository peerChatRepository;
+
+    @Override
+    public Optional<PeerChat> getChatBetweenUsers(User user1, User user2) {
+        return Optional.empty();
+    }
 }
--- src/main/java/de/othr/bib48218/chat/service/IFChatService.java
+++ src/main/java/de/othr/bib48218/chat/service/IFChatService.java
@@ -1,4 +1,10 @@
 package de.othr.bib48218.chat.service;
 
+import de.othr.bib48218.chat.entity.PeerChat;
+import de.othr.bib48218.chat.entity.User;
+
+import java.util.Optional;
+
 public interface IFChatService {
+    Optional<PeerChat> getChatBetweenUsers(User user1, User user2);
 }
