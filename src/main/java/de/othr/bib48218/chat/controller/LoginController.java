04d2b3fa4233cd1889c1268b5f40d126bb91fac6
Benno Bielmeier <github@bbenno.com>
2020-12-18 11:27:56 +0100

    Add redirect after success login

--- src/main/java/de/othr/bib48218/chat/controller/LoginController.java
+++ src/main/java/de/othr/bib48218/chat/controller/LoginController.java
@@ -4,8 +4,11 @@ import de.othr.bib48218.chat.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
@@ -18,7 +21,9 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String loginUser(Model model) {
-        return "home";
+    public ModelAndView loginUser(ModelMap model, @RequestParam("username") String username, @RequestParam("password") String password) {
+        // TODO: Check login credentials
+        model.addAttribute("login", "success");
+        return new ModelAndView("redirect:/", model);
     }
 }
