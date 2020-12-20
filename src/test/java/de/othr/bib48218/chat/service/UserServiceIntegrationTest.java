8c5ac8786dfb3a26d6b305e5b942f71a395203b2
Benno Bielmeier <github@bbenno.com>
2020-12-20 20:49:54 +0100

    Add testing for new UserService methods

--- src/test/java/de/othr/bib48218/chat/service/UserServiceIntegrationTest.java
+++ src/test/java/de/othr/bib48218/chat/service/UserServiceIntegrationTest.java
@@ -1,6 +1,8 @@
 package de.othr.bib48218.chat.service;
 
+import de.othr.bib48218.chat.entity.Bot;
 import de.othr.bib48218.chat.entity.Person;
+import de.othr.bib48218.chat.entity.User;
 import de.othr.bib48218.chat.repository.BotRepository;
 import de.othr.bib48218.chat.repository.PersonRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -32,17 +34,66 @@ class UserServiceIntegrationTest {
                 "Joe",
                 "Smith",
                 "joe@smith.com");
+        Bot bot = new Bot(
+                "botty",
+                "",
+                null);
 
         Mockito.when(personRepository.findByFirstName(joe.getFirstName())).thenReturn(joe);
+        Mockito.when(personRepository.findByUsername(joe.getUsername())).thenReturn(joe);
+        Mockito.when(botRepository.findByUsername(bot.getUsername())).thenReturn(bot);
     }
 
     @Test
     void whenValidFirstName_thenPersonShouldBeFound() {
+        // given
         String firstName = "Joe";
+
+        // when
         Person found = userService.getPersonByFirstName(firstName);
 
+        // then
         assertThat(found).isNotNull();
         assertThat(found.getFirstName()).isEqualTo(firstName);
+    }
+
+    @Test
+    void whenValidBotUsername_thenBotShouldBeFound() {
+        // given
+        String username = "botty";
+
+        // when
+        Bot found = userService.getBotByUsername(username);
+
+        // then
+        assertThat(found).isNotNull();
+        assertThat(found.getUsername()).isEqualTo(username);
+    }
+
+    @Test
+    void whenValidPersonUsername_thenPersonShouldBeFound() {
+        // given
+        String username = "joe";
+
+        // when
+        Person found = userService.getPersonByUsername(username);
+
+        // then
+        assertThat(found).isNotNull();
+        assertThat(found.getUsername()).isEqualTo(username);
+    }
+
+    @Test
+    void whenValidUsername_thenUserShouldBeFound() {
+        // given
+        String username = "botty";
+
+        // when
+        User found = userService.getUserByUsername(username);
+
+        // then
+        assertThat(found).isNotNull();
+        assertThat(found.getUsername()).isEqualTo(username);
     }
 
     @TestConfiguration
