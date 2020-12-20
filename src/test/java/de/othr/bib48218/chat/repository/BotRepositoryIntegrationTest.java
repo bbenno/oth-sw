973cb4d213d89b0ee5dd35067e5b6e8a07d939c6
Benno Bielmeier <github@bbenno.com>
2020-12-20 19:12:25 +0100

    Add AutoConfigureTestDatabase annotation
    This prevents usage of in-memory database (causing error, due to not
    part of pom.xml). Example of an in-memory database is h2.
    The Option `Replace.NONE` means that the default configuration and the
    default database is not replaced (by in-memory one).

--- src/test/java/de/othr/bib48218/chat/repository/BotRepositoryIntegrationTest.java
+++ src/test/java/de/othr/bib48218/chat/repository/BotRepositoryIntegrationTest.java
@@ -3,12 +3,14 @@ package de.othr.bib48218.chat.repository;
 import de.othr.bib48218.chat.entity.Bot;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class BotRepositoryIntegrationTest {
     @Autowired
     private TestEntityManager entityManager;
