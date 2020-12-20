19381e02b1cb31473d983dbdc850860654158ef2
Benno Bielmeier <github@bbenno.com>
2020-12-20 20:44:23 +0100

    Add getXByUsername methods to UserService

--- src/main/java/de/othr/bib48218/chat/service/UserService.java
+++ src/main/java/de/othr/bib48218/chat/service/UserService.java
@@ -1,6 +1,9 @@
 package de.othr.bib48218.chat.service;
 
+import de.othr.bib48218.chat.entity.Bot;
 import de.othr.bib48218.chat.entity.Person;
+import de.othr.bib48218.chat.entity.User;
+import de.othr.bib48218.chat.repository.BotRepository;
 import de.othr.bib48218.chat.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,8 +13,27 @@ public class UserService implements IFUserService {
     @Autowired
     private PersonRepository personRepository;
 
+    @Autowired
+    private BotRepository botRepository;
+
     @Override
     public Person getPersonByFirstName(String firstName) {
         return personRepository.findByFirstName(firstName);
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        User found = getPersonByUsername(username);
+        return (found != null) ? found : getBotByUsername(username);
+    }
+
+    @Override
+    public Person getPersonByUsername(String username) {
+        return personRepository.findByUsername(username);
+    }
+
+    @Override
+    public Bot getBotByUsername(String username) {
+        return botRepository.findByUsername(username);
     }
 }
