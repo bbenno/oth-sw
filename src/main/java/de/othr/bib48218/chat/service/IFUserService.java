aa9f61679b12aa908bb3e1b72bc1cc98f711a0f7
Benno Bielmeier <github@bbenno.com>
2020-12-20 13:16:02 +0100

    Add getPersonByFirstName() to UserService

--- src/main/java/de/othr/bib48218/chat/service/IFUserService.java
+++ src/main/java/de/othr/bib48218/chat/service/IFUserService.java
@@ -1,4 +1,7 @@
 package de.othr.bib48218.chat.service;
 
+import de.othr.bib48218.chat.entity.Person;
+
 public interface IFUserService {
+    Person getPersonByFirstName(String firstName);
 }
--- src/main/java/de/othr/bib48218/chat/service/UserService.java
+++ src/main/java/de/othr/bib48218/chat/service/UserService.java
@@ -1,13 +1,17 @@
 package de.othr.bib48218.chat.service;
 
-import de.othr.bib48218.chat.repository.UserRepository;
+import de.othr.bib48218.chat.entity.Person;
+import de.othr.bib48218.chat.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService implements IFUserService {
-    @Qualifier("personRepository")
     @Autowired
-    private UserRepository repository;
+    private PersonRepository personRepository;
+
+    @Override
+    public Person getPersonByFirstName(String firstName) {
+        return personRepository.findByFirstName(firstName);
+    }
 }
