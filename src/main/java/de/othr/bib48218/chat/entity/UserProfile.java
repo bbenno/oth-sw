b93a92b3c9d28c31b16bd3c0270a61a5ed5fc03c
Benno Bielmeier <github@bbenno.com>
2020-12-20 21:56:34 +0100

    Replace UserProfile's constructors with lombok annotation

--- src/main/java/de/othr/bib48218/chat/entity/UserProfile.java
+++ src/main/java/de/othr/bib48218/chat/entity/UserProfile.java
@@ -1,19 +1,14 @@
 package de.othr.bib48218.chat.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class UserProfile extends IdEntity {
     private String bio;
     private String country;
-
-    protected UserProfile() {
-    }
-
-    public UserProfile(String bio, String country) {
-        this.bio = bio;
-        this.country = country;
-    }
 }
