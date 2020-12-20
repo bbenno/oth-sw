8c6907a3b1a369e766a218e32a41756a243111ae
Benno Bielmeier <github@bbenno.com>
2020-12-20 12:44:29 +0100

    Add findByFirstName to PersonResository

--- src/main/java/de/othr/bib48218/chat/repository/PersonRepository.java
+++ src/main/java/de/othr/bib48218/chat/repository/PersonRepository.java
@@ -3,4 +3,5 @@ package de.othr.bib48218.chat.repository;
 import de.othr.bib48218.chat.entity.Person;
 
 public interface PersonRepository extends UserRepository<Person> {
+    Person findByFirstName(String firstName);
 }
