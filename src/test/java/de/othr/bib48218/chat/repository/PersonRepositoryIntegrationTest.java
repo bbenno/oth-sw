1d96603b463d79a7a58da2b1e130aece6ac934b8
Benno Bielmeier <github@bbenno.com>
2020-12-20 14:07:04 +0100

    Add test for findByUsername of PersonRepository

--- src/test/java/de/othr/bib48218/chat/repository/PersonRepositoryIntegrationTest.java
+++ src/test/java/de/othr/bib48218/chat/repository/PersonRepositoryIntegrationTest.java
@@ -41,4 +41,27 @@ public class PersonRepositoryIntegrationTest {
         assertThat(found.getFirstName()).isEqualTo(joe.getFirstName());
         assertThat(found).isEqualTo(joe);
     }
+
+    @Test
+    public void whenFindByUsername_thenReturnPerson() {
+        // given
+        Person joe = new Person(
+                "joe",
+                "",
+                null,
+                "Joe",
+                "Smith",
+                "joe@smith.com");
+
+        entityManager.persist(joe);
+        entityManager.flush();
+
+        // when
+        Person found = personRepository.findByUsername(joe.getUsername());
+
+        // then
+        assertThat(found).isNotNull();
+        assertThat(found.getUsername()).isEqualTo(joe.getUsername());
+        assertThat(found).isEqualTo(joe);
+    }
 }
