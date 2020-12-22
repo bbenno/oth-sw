829edd77df488bca2a5952677e49e11b859ef8c0
Benno Bielmeier <github@bbenno.com>
2020-12-22 16:09:24 +0100

    Add NonNull annotation to Message member

--- src/main/java/de/othr/bib48218/chat/entity/Message.java
+++ src/main/java/de/othr/bib48218/chat/entity/Message.java
@@ -2,6 +2,7 @@ package de.othr.bib48218.chat.entity;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.lang.NonNull;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToOne;
@@ -11,11 +12,14 @@ import java.time.ZonedDateTime;
 @Getter
 @NoArgsConstructor
 public class Message extends IdEntity {
+    @NonNull
     private String text;
+    @NonNull
     private ZonedDateTime timestamp;
     @OneToOne
     private Attachment attachment;
     @OneToOne
+    @NonNull
     private User author;
     @OneToOne
     private Message replyOf;
