afec4f68ee816278a715ec3bf96122f7bd6bf829
Benno Bielmeier <github@bbenno.com>
2020-12-22 15:56:29 +0100

    Add setters to Profile and ChatProfile

--- src/main/java/de/othr/bib48218/chat/entity/ChatProfile.java
+++ src/main/java/de/othr/bib48218/chat/entity/ChatProfile.java
@@ -2,12 +2,14 @@ package de.othr.bib48218.chat.entity;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.lang.NonNull;
 
 import javax.persistence.Entity;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class ChatProfile extends Profile {
     @NonNull
--- src/main/java/de/othr/bib48218/chat/entity/Profile.java
+++ src/main/java/de/othr/bib48218/chat/entity/Profile.java
@@ -2,12 +2,14 @@ package de.othr.bib48218.chat.entity;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.lang.NonNull;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Profile {
