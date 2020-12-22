1b92bf35aff1c4e9c836d294d4fad4ce9233f910
Benno Bielmeier <github@bbenno.com>
2020-12-22 11:06:38 +0100

    Add Setter to Attachment entity

--- src/main/java/de/othr/bib48218/chat/entity/Attachment.java
+++ src/main/java/de/othr/bib48218/chat/entity/Attachment.java
@@ -2,11 +2,13 @@ package de.othr.bib48218.chat.entity;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class Attachment extends IdEntity {
     private int size;
