ca10da7464b7383edbadf1ce25b5e6514d65ee44
Benno Bielmeier <github@bbenno.com>
2020-12-22 17:17:11 +0100

    Refactor PersonRepositoryIntegrationTest

--- src/test/java/de/othr/bib48218/chat/repository/PersonRepositoryIntegrationTest.java
+++ src/test/java/de/othr/bib48218/chat/repository/PersonRepositoryIntegrationTest.java
@@ -1,6 +1,7 @@
 package de.othr.bib48218.chat.repository;
 
 import de.othr.bib48218.chat.entity.Person;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -8,33 +9,33 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class PersonRepositoryIntegrationTest {
+    private final Person joe = new Person(
+        "joe",
+        "",
+        "Joe",
+        "Smith",
+        "joe@smith.com");
     @Autowired
     private TestEntityManager entityManager;
 
     @Autowired
     private PersonRepository personRepository;
 
-    @Test
-    void whenFindByFirstName_thenReturnPerson() {
-        // given
-        Person joe = new Person(
-                "joe",
-                "",
-                "Joe",
-                "Smith",
-                "joe@smith.com");
-
+    @BeforeEach
+    void setup() {
         entityManager.persist(joe);
         entityManager.flush();
+    }
 
-        // when
+    @Test
+    void whenFindByFirstName_thenReturnPerson() {
         Person found = personRepository.findByFirstName(joe.getFirstName());
 
-        // then
         assertThat(found).isNotNull();
         assertThat(found.getFirstName()).isEqualTo(joe.getFirstName());
         assertThat(found).isEqualTo(joe);
@@ -42,21 +43,8 @@ class PersonRepositoryIntegrationTest {
 
     @Test
     void whenFindByUsername_thenReturnPerson() {
-        // given
-        Person joe = new Person(
-                "joe",
-                "",
-                "Joe",
-                "Smith",
-                "joe@smith.com");
-
-        entityManager.persist(joe);
-        entityManager.flush();
-
-        // when
         Person found = personRepository.findByUsername(joe.getUsername());
 
-        // then
         assertThat(found).isNotNull();
         assertThat(found.getUsername()).isEqualTo(joe.getUsername());
         assertThat(found).isEqualTo(joe);
