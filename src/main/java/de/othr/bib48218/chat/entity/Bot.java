ad1d63019f6bd58407ffc08d6b874270bf376165
Benno Bielmeier <32938211+bbenno@users.noreply.github.com>
2020-12-22 13:30:48 +0100

    Test/bot (#11)
    * Add setter for User members
    
    * Update member annotations
    
    Use spring's NonNull annotation in preference to lombok's one.
    
    * Add BotTest
    
    * Add @Setter annotation

--- src/main/java/de/othr/bib48218/chat/entity/Bot.java
+++ src/main/java/de/othr/bib48218/chat/entity/Bot.java
@@ -2,11 +2,13 @@ package de.othr.bib48218.chat.entity;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class Bot extends User {
     public Bot(String username, String password) {
