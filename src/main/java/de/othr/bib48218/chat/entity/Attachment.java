7cc3b9b398b44f12707bc6664dba03b697d6914d
Benno Bielmeier <32938211+bbenno@users.noreply.github.com>
2020-12-22 11:19:12 +0100

    Test/attachment (#10)
    * Add Setter to Attachment entity
    
    * Add Non-Null unit tests for Attachment
    
    * Add @NonNull to Attachment's members
    
    * Add AllArgsConstructor to Attachment
    
    * Add validAttachment to test

--- src/main/java/de/othr/bib48218/chat/entity/Attachment.java
+++ src/main/java/de/othr/bib48218/chat/entity/Attachment.java
@@ -1,15 +1,21 @@
 package de.othr.bib48218.chat.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.lang.NonNull;
 
 import javax.persistence.Entity;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Attachment extends IdEntity {
-    private int size;
+    @NonNull
     private String name;
+    @NonNull
     private String mimeType;
 }
