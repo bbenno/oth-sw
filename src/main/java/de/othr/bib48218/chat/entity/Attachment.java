506fe3bf26630c2c990d71d58f7fb403f236630a
Benno Bielmeier <github@bbenno.com>
2020-12-22 11:08:29 +0100

    Add @NonNull to Attachment's members

--- src/main/java/de/othr/bib48218/chat/entity/Attachment.java
+++ src/main/java/de/othr/bib48218/chat/entity/Attachment.java
@@ -3,6 +3,7 @@ package de.othr.bib48218.chat.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.lang.NonNull;
 
 import javax.persistence.Entity;
 
@@ -11,7 +12,8 @@ import javax.persistence.Entity;
 @Setter
 @NoArgsConstructor
 public class Attachment extends IdEntity {
-    private int size;
+    @NonNull
     private String name;
+    @NonNull
     private String mimeType;
 }
