706c802888518b93ce8cf35dacf2e42f0f7c834f
Benno Bielmeier <github@bbenno.com>
2020-12-22 15:37:09 +0100

    Add Setter of ChatMembership

--- src/main/java/de/othr/bib48218/chat/entity/ChatMembership.java
+++ src/main/java/de/othr/bib48218/chat/entity/ChatMembership.java
@@ -3,6 +3,7 @@ package de.othr.bib48218.chat.entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.lang.NonNull;
 
 import javax.persistence.Entity;
@@ -10,6 +11,7 @@ import javax.persistence.OneToOne;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChatMembership extends IdEntity {
