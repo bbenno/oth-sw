7909f127378656507b35ceef736688cdd260b276
Benno Bielmeier <github@bbenno.com>
2020-12-22 16:00:23 +0100

    Add setter to GroupChat

--- src/main/java/de/othr/bib48218/chat/entity/GroupChat.java
+++ src/main/java/de/othr/bib48218/chat/entity/GroupChat.java
@@ -2,12 +2,14 @@ package de.othr.bib48218.chat.entity;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToOne;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class GroupChat extends Chat {
     private GroupVisibility visibility;
