f3484d8127cac7d1f2a26bfd37009b6a9e5e8445
Benno Bielmeier <github@bbenno.com>
2020-12-22 15:34:08 +0100

    Fix ChatProfile inheritage

--- src/main/java/de/othr/bib48218/chat/entity/ChatProfile.java
+++ src/main/java/de/othr/bib48218/chat/entity/ChatProfile.java
@@ -2,12 +2,19 @@ package de.othr.bib48218.chat.entity;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.lang.NonNull;
 
 import javax.persistence.Entity;
 
 @Entity
 @Getter
 @NoArgsConstructor
-public class ChatProfile extends IdEntity {
+public class ChatProfile extends Profile {
+    @NonNull
     private String description;
+
+    public ChatProfile(final String name, final String description) {
+        super(name);
+        this.description = description;
+    }
 }
