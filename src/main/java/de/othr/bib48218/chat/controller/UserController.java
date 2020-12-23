45cdbf1075d7676e24eb7a568ccc57540899f8a8
Benno Bielmeier <github@bbenno.com>
2020-12-23 13:24:52 +0100

    Add PostMapping for createPerson

--- src/main/java/de/othr/bib48218/chat/controller/UserController.java
+++ src/main/java/de/othr/bib48218/chat/controller/UserController.java
@@ -2,6 +2,8 @@ package de.othr.bib48218.chat.controller;
 
 import de.othr.bib48218.chat.entity.Person;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,5 +13,10 @@ public class UserController {
     @RequestMapping("/new")
     public ModelAndView showPersonForm() {
         return new ModelAndView("user/new_person", "person", new Person());
+    }
+
+    @PostMapping("/new")
+    public ModelAndView createPerson(@ModelAttribute("person") Person person) {
+        return new ModelAndView("redirect:" + person.getUsername());
     }
 }
