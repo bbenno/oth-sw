c35114800b8d644107a11047cf1d0a04791df3ec
Benno Bielmeier <github@bbenno.com>
2020-12-23 13:44:07 +0100

    Add UserControllerTest

---
+++ src/test/java/de/othr/bib48218/chat/controller/UserControllerTest.java
@@ -0,0 +1,37 @@
+package de.othr.bib48218.chat.controller;
+
+import de.othr.bib48218.chat.entity.Person;
+import de.othr.bib48218.chat.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    void personFormShowTest() throws Exception {
+        mvc.perform(get("/user/new"))
+            .andExpect(model().attribute("person", any(Person.class)))
+            .andExpect(view().name("user/new_person"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    void personFormPostTest() throws Exception {
+        mvc.perform(post("/user/new").param("username", "test_user").param("password", "abc"))
+            .andExpect(status().is3xxRedirection());
+    }
+}
