9becb140f4929645b8b53ce9a47d4449e3af3574
Benno Bielmeier <github@bbenno.com>
2020-12-23 15:00:09 +0100

    Add show User to UserController

--- src/main/java/de/othr/bib48218/chat/controller/UserController.java
+++ src/main/java/de/othr/bib48218/chat/controller/UserController.java
@@ -1,8 +1,13 @@
 package de.othr.bib48218.chat.controller;
 
 import de.othr.bib48218.chat.entity.Person;
+import de.othr.bib48218.chat.entity.User;
+import de.othr.bib48218.chat.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,13 +15,32 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/new")
     public ModelAndView showPersonForm() {
         return new ModelAndView("user/new_person", "person", new Person());
     }
 
     @PostMapping("/new")
-    public ModelAndView createPerson(@ModelAttribute("person") Person person) {
-        return new ModelAndView("redirect:" + person.getUsername());
+    public ModelAndView createPerson(@Validated Person person, BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return new ModelAndView("user/new_person", "person", person);
+        else {
+            userService.createPerson(person);
+            return new ModelAndView("redirect:" + person.getUsername());
+        }
+    }
+
+    @RequestMapping("{username}")
+    public ModelAndView showUser(@PathVariable String username) {
+        User found = userService.getUserByUsername(username);
+        if (found != null && found instanceof Person) {
+            Person person = (Person) found;
+            return new ModelAndView("user/show_person", "person", found);
+        } else {
+            return new ModelAndView("redirect:new");
+        }
     }
 }
