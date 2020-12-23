eb5e94ffc1180c2f01f11b180e61d9be00e27b9f
Benno Bielmeier <github@bbenno.com>
2020-12-23 18:22:57 +0100

    Override ChatRepository#findById

--- src/main/java/de/othr/bib48218/chat/repository/GroupChatRepository.java
+++ src/main/java/de/othr/bib48218/chat/repository/GroupChatRepository.java
@@ -2,5 +2,9 @@ package de.othr.bib48218.chat.repository;
 
 import de.othr.bib48218.chat.entity.GroupChat;
 
+import java.util.Optional;
+
 public interface GroupChatRepository extends ChatRepository<GroupChat> {
+    @Override
+    Optional<GroupChat> findById(Long id);
 }
