7de3c4cd7dae075f529eabbdcf9be6148346c301
Benno Bielmeier <github@bbenno.com>
2020-12-23 18:24:07 +0100

    Add getChatById to ChatService

--- src/main/java/de/othr/bib48218/chat/service/ChatService.java
+++ src/main/java/de/othr/bib48218/chat/service/ChatService.java
@@ -1,13 +1,24 @@
 package de.othr.bib48218.chat.service;
 
+import de.othr.bib48218.chat.entity.Chat;
+import de.othr.bib48218.chat.entity.PeerChat;
 import de.othr.bib48218.chat.repository.ChatRepository;
+import de.othr.bib48218.chat.repository.GroupChatRepository;
+import de.othr.bib48218.chat.repository.PeerChatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatService implements IFChatService {
-    @Qualifier("groupChatRepository")
     @Autowired
-    private ChatRepository repository;
+    private GroupChatRepository groupRepository;
+
+    @Autowired
+    private PeerChatRepository peerRepository;
+
+    @Override
+    public Chat getChatById(Long id) {
+        return groupRepository.findById(id).get();
+    }
 }
