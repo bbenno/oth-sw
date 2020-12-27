92ac2f4a5443a99520b28e1db578dae9bbbd25aa
Benno Bielmeier <github@bbenno.com>
2020-12-27 15:31:39 +0100

    Trim Strings in RegisterController

--- src/main/java/de/othr/bib48218/chat/controller/RegisterController.java
+++ src/main/java/de/othr/bib48218/chat/controller/RegisterController.java
@@ -3,12 +3,17 @@ package de.othr.bib48218.chat.controller;
 import de.othr.bib48218.chat.entity.Person;
 import de.othr.bib48218.chat.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 @RequestMapping("/register")
 public class RegisterController {
     @Autowired
@@ -27,5 +32,11 @@ public class RegisterController {
             userService.createPerson(person);
             return new ModelAndView("redirect:/user/" + person.getUsername());
         }
+    }
+
+    @InitBinder
+    void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
     }
 }
