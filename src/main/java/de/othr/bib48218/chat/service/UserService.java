82f8c4487c2af283abffb186f063e35a552f838a
Benno Bielmeier <github@bbenno.com>
2020-12-27 11:00:15 +0100

    Refactor UserService

--- src/main/java/de/othr/bib48218/chat/service/UserService.java
+++ src/main/java/de/othr/bib48218/chat/service/UserService.java
@@ -6,7 +6,6 @@ import de.othr.bib48218.chat.entity.User;
 import de.othr.bib48218.chat.repository.BotRepository;
 import de.othr.bib48218.chat.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
