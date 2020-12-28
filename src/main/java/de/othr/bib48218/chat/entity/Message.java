8714c56bcb5dbfb742ddcf19fd0d4e00d677c6da
Benno Bielmeier <github@bbenno.com>
2020-12-28 10:59:02 +0100

    Add chat member to message

--- src/main/java/de/othr/bib48218/chat/entity/Message.java
+++ src/main/java/de/othr/bib48218/chat/entity/Message.java
@@ -1,59 +1,76 @@
 package de.othr.bib48218.chat.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.lang.NonNull;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Message extends IdEntity {
     @NonNull
+    @lombok.NonNull
+    @NotNull
+    @NotBlank
     private String text;
+
+    @OneToOne(cascade = CascadeType.ALL)
     @NonNull
-    private LocalDateTime timestamp;
-    @OneToOne
-    private Attachment attachment;
-    @OneToOne
+    @lombok.NonNull
+    @NotNull
+    private Chat chat;
+
+    @OneToOne(cascade = CascadeType.ALL)
     @NonNull
+    @lombok.NonNull
+    @NotNull
     private User author;
+
+    @NonNull
+    @lombok.NonNull
+    @NotNull
+    private LocalDateTime timestamp;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Attachment attachment;
+
     @OneToOne
     private Message replyOf;
 
     public Message(@NonNull String text,
+                   @NonNull Chat chat,
                    @NonNull User author,
                    @NonNull LocalDateTime timestamp) {
         this.text = text;
+        this.chat = chat;
         this.author = author;
         this.timestamp = timestamp;
     }
 
     public Message(@NonNull String text,
+                   @NonNull Chat chat,
                    @NonNull User author,
                    @NonNull LocalDateTime timestamp,
                    @NonNull Attachment attachment) {
-        this(text, author, timestamp);
+        this(text, chat, author, timestamp);
         this.attachment = attachment;
     }
 
     public Message(@NonNull String text,
-                   @NonNull User author,
-                   @NonNull LocalDateTime timestamp,
-                   @NonNull Attachment attachment,
-                   @NonNull Message replyOf) {
-        this(text, author, timestamp, attachment);
-        this.replyOf = replyOf;
-    }
-
-    public Message(@NonNull String text,
+                   @NonNull Chat chat,
                    @NonNull User author,
                    @NonNull LocalDateTime timestamp,
                    @NonNull Message replyOf) {
-        this(text, author, timestamp);
+        this(text, chat, author, timestamp);
         this.replyOf = replyOf;
     }
 }
