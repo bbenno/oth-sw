f6f3c361f5437f3e1a937789114adc78315e90ee
Benno Bielmeier <github@bbenno.com>
2020-12-28 15:06:20 +0100

    Add getChatsByUser to ChatService

--- src/main/java/de/othr/bib48218/chat/service/IFChatService.java
+++ src/main/java/de/othr/bib48218/chat/service/IFChatService.java
@@ -1,7 +1,12 @@
 package de.othr.bib48218.chat.service;
 
 import de.othr.bib48218.chat.entity.Chat;
+import de.othr.bib48218.chat.entity.User;
+
+import java.util.Collection;
 
 public interface IFChatService {
     Chat getChatById(Long id);
+
+    Collection<Chat> getChatsByUser(User user);
 }
