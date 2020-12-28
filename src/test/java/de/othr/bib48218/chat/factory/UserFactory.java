45a96b59dc57f9bb8037c52e3f3e69d0160dca1e
Benno Bielmeier <github@bbenno.com>
2020-12-28 15:47:02 +0100

    Add factory method to UserFactory

--- src/test/java/de/othr/bib48218/chat/factory/UserFactory.java
+++ src/test/java/de/othr/bib48218/chat/factory/UserFactory.java
@@ -8,7 +8,18 @@ public class UserFactory {
     private static final Faker faker = new Faker();
 
     public static Person newValidPerson() {
-        String username = faker.name().username();
+        return newValidPersonWithUsername(username());
+    }
+
+    public static Bot newValidBot() {
+        return newValidBotWithUsername(username());
+    }
+
+    public static Bot newValidBotWithUsername(String username) {
+        return new Bot(username, password());
+    }
+
+    public static Person newValidPersonWithUsername(String username) {
         String firstName = faker.name().firstName();
         String lastName = faker.name().lastName();
         String password = password();
@@ -17,18 +28,11 @@ public class UserFactory {
         return new Person(username, password, firstName, lastName, email);
     }
 
-    public static Bot newValidBot() {
-        String username = faker.name().username();
-        String password = password();
-
-        return new Bot(username, password);
-    }
-
-    public static Bot newValidBotWithUsername(String username) {
-        return new Bot(username, password());
-    }
-
     private static String password() {
         return faker.internet().password(8, 20);
+    }
+
+    private static String username() {
+        return faker.name().username();
     }
 }
