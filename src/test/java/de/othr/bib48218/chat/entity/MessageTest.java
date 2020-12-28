b8f72c56a426046622832ee8ea3b418f4c02bc73
Benno Bielmeier <github@bbenno.com>
2020-12-28 11:55:39 +0100

    Update entity unit tests
    * Add Test for present NoArgsConstructor
    * Add constructor NonNull constraints

--- src/test/java/de/othr/bib48218/chat/entity/MessageTest.java
+++ src/test/java/de/othr/bib48218/chat/entity/MessageTest.java
@@ -1,4 +1,78 @@
 package de.othr.bib48218.chat.entity;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.ThrowingSupplier;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class MessageTest {
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    void shouldHaveAuthor() {
+        var message = new Message();
+
+        assertThrows(NullPointerException.class, () -> message.setAuthor(null));
+        assertThrows(NullPointerException.class, () -> new Message(
+            "text",
+            new Chat() {
+            },
+            null,
+            LocalDateTime.now()
+            ));
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    void shouldHaveText() {
+        var message = new Message();
+
+        assertThrows(NullPointerException.class, () -> message.setText(null));
+        assertThrows(NullPointerException.class, () -> new Message(
+            null,
+            new Chat() {
+            },
+            new User() {
+            },
+            LocalDateTime.now()
+        ));
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    void shouldHaveChat() {
+        var message = new Message();
+
+        assertThrows(NullPointerException.class, () -> message.setChat(null));
+        assertThrows(NullPointerException.class, () -> new Message(
+            "text",
+            null,
+            new User() {
+            },
+            LocalDateTime.now()
+        ));
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    void shouldHaveTimestamp() {
+        var message = new Message();
+
+        assertThrows(NullPointerException.class, () -> message.setTimestamp(null));
+        assertThrows(NullPointerException.class, () -> new Message(
+            "text",
+            new Chat() {
+            },
+            new User() {
+            },
+            null
+        ));
+    }
+
+    @Test
+    void shouldHaveNoArgsConstructor() {
+        assertDoesNotThrow((ThrowingSupplier<Message>) Message::new);
+    }
 }
--- src/test/java/de/othr/bib48218/chat/entity/UserProfileTest.java
+++ src/test/java/de/othr/bib48218/chat/entity/UserProfileTest.java
@@ -1,15 +1,24 @@
 package de.othr.bib48218.chat.entity;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.ThrowingSupplier;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UserProfileTest {
+
+    @SuppressWarnings("ConstantConditions")
     @Test
     void shouldHaveName() {
         var chatProfile = new ChatProfile();
 
-        // noinspection ConstantConditions
         assertThrows(NullPointerException.class, () -> chatProfile.setName(null));
+        assertThrows(NullPointerException.class, () -> new ChatProfile(null, "description"));
+    }
+
+    @Test
+    void shouldHaveNoArgsConstructor() {
+        assertDoesNotThrow((ThrowingSupplier<UserProfile>) UserProfile::new);
     }
 }
