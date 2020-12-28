74c2669a65c7d0c8430e6c7f25050e6e10a8c80b
Benno Bielmeier <github@bbenno.com>
2020-12-28 18:26:58 +0100

    Add shouldFindMessagesByChat test

--- src/test/java/de/othr/bib48218/chat/repository/MessageRepositoryIntegrationTest.java
+++ src/test/java/de/othr/bib48218/chat/repository/MessageRepositoryIntegrationTest.java
@@ -1,17 +1,20 @@
 package de.othr.bib48218.chat.repository;
 
-import de.othr.bib48218.chat.entity.*;
+import de.othr.bib48218.chat.entity.Chat;
+import de.othr.bib48218.chat.entity.Message;
+import de.othr.bib48218.chat.entity.User;
+import de.othr.bib48218.chat.factory.ChatFactory;
+import de.othr.bib48218.chat.factory.MessageFactory;
+import de.othr.bib48218.chat.factory.UserFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-import java.time.LocalDateTime;
 import java.util.Collection;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -24,14 +27,29 @@ public class MessageRepositoryIntegrationTest {
 
     @Test
     void shouldFindMessagesByAuthor() {
-        Chat chat = new GroupChat();
-        User author = new Person("person_1", "password");
-        Message message = new Message("Hello world", chat, author, LocalDateTime.now());
-        entityManager.persistAndFlush(message);
+        Chat chat = ChatFactory.newValidGroupChat();
+        User author = UserFactory.newValidPerson();
+        Message message = MessageFactory.newMessage(chat, author);
+        message = entityManager.persistAndFlush(message);
 
         Collection<Message> messages = messageRepository.findByAuthor(author);
 
         assertThat(messages).isNotNull();
+        assertThat(messages).isNotEmpty();
+        assertThat(messages).contains(message);
+    }
+
+    @Test
+    void shouldFindMessagesByChat() {
+        Chat chat = ChatFactory.newValidGroupChat();
+        User author = UserFactory.newValidPerson();
+        Message message = MessageFactory.newMessage(chat, author);
+        message = entityManager.persistAndFlush(message);
+
+        Collection<Message> messages = messageRepository.findByChat(chat);
+
+        assertThat(messages).isNotNull();
+        assertThat(messages).isNotEmpty();
         assertThat(messages).contains(message);
     }
 }
