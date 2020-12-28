2c4277f2aac8926494e86db87cea9a51548fe63b
Benno Bielmeier <github@bbenno.com>
2020-12-28 10:31:52 +0100

    Add message member to Attachement

--- src/main/java/de/othr/bib48218/chat/entity/Attachment.java
+++ src/main/java/de/othr/bib48218/chat/entity/Attachment.java
@@ -1,21 +1,35 @@
 package de.othr.bib48218.chat.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.lang.NonNull;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(force = true)
 public class Attachment extends IdEntity {
     @NonNull
+    @lombok.NonNull
+    @NotBlank
     private String name;
+
     @NonNull
+    @lombok.NonNull
+    @NotBlank
     private String mimeType;
+
+    @NonNull
+    @OneToOne(mappedBy = "attachment")
+    private Message message;
+
+    public Attachment(@lombok.NonNull @NonNull String name, @lombok.NonNull @NonNull String mimeType) {
+        this.name = name;
+        this.mimeType = mimeType;
+    }
 }
