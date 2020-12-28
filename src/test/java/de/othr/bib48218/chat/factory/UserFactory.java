aa3d9ba617fcff8e311db0c0a655eaf4bf93a257
Benno Bielmeier <github@bbenno.com>
2020-12-28 15:39:13 +0100

    Add factory methods for Bot

--- src/test/java/de/othr/bib48218/chat/factory/UserFactory.java
+++ src/test/java/de/othr/bib48218/chat/factory/UserFactory.java
@@ -1,6 +1,7 @@
 package de.othr.bib48218.chat.factory;
 
 import com.github.javafaker.Faker;
+import de.othr.bib48218.chat.entity.Bot;
 import de.othr.bib48218.chat.entity.Person;
 
 public class UserFactory {
@@ -10,9 +11,24 @@ public class UserFactory {
         String username = faker.name().username();
         String firstName = faker.name().firstName();
         String lastName = faker.name().lastName();
-        String password = faker.internet().password();
+        String password = password();
         String email = faker.internet().emailAddress();
 
         return new Person(username, password, firstName, lastName, email);
+    }
+
+    public static Bot newValidBot() {
+        String username = faker.name().username();
+        String password = password();
+
+        return new Bot(username, password);
+    }
+
+    public static Bot newValidBotWithUsername(String username) {
+        return new Bot(username, password());
+    }
+
+    private static String password() {
+        return faker.internet().password(8, 20);
     }
 }
