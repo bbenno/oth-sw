24be535359b45c01c5f1d713fd6bc12b9103ed38
Benno Bielmeier <github@bbenno.com>
2020-12-31 19:34:55 +0100

    Add ChatMembershipRepository

---
+++ src/test/java/de/othr/bib48218/chat/repository/ChatMembershipRepositoryIntegrationTest.java
@@ -0,0 +1,51 @@
+package de.othr.bib48218.chat.repository;
+
+import de.othr.bib48218.chat.entity.Chat;
+import de.othr.bib48218.chat.entity.ChatMembership;
+import de.othr.bib48218.chat.entity.User;
+import de.othr.bib48218.chat.factory.ChatFactory;
+import de.othr.bib48218.chat.factory.ChatMembershipFactory;
+import de.othr.bib48218.chat.factory.UserFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class ChatMembershipRepositoryIntegrationTest {
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private ChatMembershipRepository chatMembershipRepository;
+
+    @Test
+    void shouldFindChatMembershipsByUser() {
+        User user = UserFactory.newValidPerson();
+        ChatMembership chatMembership = ChatMembershipFactory.newChatMembershipWithUser(user);
+        entityManager.persistAndFlush(chatMembership);
+
+        var found = chatMembershipRepository.findByUser(user);
+
+        assertThat(found).isNotNull();
+        assertThat(found).isNotEmpty();
+        assertThat(found).contains(chatMembership);
+    }
+
+    @Test
+    void shouldFindChatMembershipsByChat() {
+        Chat chat = ChatFactory.newValidGroupChat();
+        ChatMembership chatMembership = ChatMembershipFactory.newChatMembershipWithChat(chat);
+        entityManager.persistAndFlush(chatMembership);
+
+        var found = chatMembershipRepository.findByChat(chat);
+
+        assertThat(found).isNotNull();
+        assertThat(found).isNotEmpty();
+        assertThat(found).contains(chatMembership);
+    }
+}
