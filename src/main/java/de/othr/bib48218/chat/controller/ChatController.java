ef142f07e1a2a028144016aed077a7110f4fd046
Benno Bielmeier <github@bbenno.com>
2020-12-31 20:57:37 +0100

    Add /chat/{id}/add handling

--- src/main/java/de/othr/bib48218/chat/controller/ChatController.java
+++ src/main/java/de/othr/bib48218/chat/controller/ChatController.java
@@ -1,12 +1,18 @@
 package de.othr.bib48218.chat.controller;
 
 import de.othr.bib48218.chat.entity.Chat;
+import de.othr.bib48218.chat.entity.User;
 import de.othr.bib48218.chat.service.IFChatService;
+import de.othr.bib48218.chat.service.IFUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/chat")
@@ -14,9 +20,26 @@ public class ChatController {
     @Autowired
     private IFChatService chatService;
 
+    @Autowired
+    private IFUserService userService;
+
     @RequestMapping("/{id}")
     public ModelAndView showChat(@PathVariable Long id) {
         Chat chat = chatService.getChatById(id);
-        return new ModelAndView("chat/show", "chat", chat);
+        return new ModelAndView("/chat/show", "chat", chat);
+    }
+
+    @RequestMapping("/{id}/add")
+    public ModelAndView addChatMember(@PathVariable long id, Principal principal) {
+        Chat chat = chatService.getChatById(id);
+        return new ModelAndView("chat/add_member", "chat", chat);
+    }
+
+    @PostMapping("/{id}/add")
+    public ModelAndView addChatMember(@PathVariable Long id, @RequestParam String username) {
+        Chat chat = chatService.getChatById(id);
+        User user = userService.getUserByUsername(username);
+        chatService.addUserToChat(user, chat);
+        return new ModelAndView("redirect:/chat/" + chat.getId());
     }
 }
---
+++ src/test/java/de/othr/bib48218/chat/controller/ChatControllerTest.java
@@ -0,0 +1,67 @@
+package de.othr.bib48218.chat.controller;
+
+import com.github.javafaker.Faker;
+import de.othr.bib48218.chat.HeaderSearchElementFactory;
+import de.othr.bib48218.chat.WebSecurityTestConfig;
+import de.othr.bib48218.chat.entity.Chat;
+import de.othr.bib48218.chat.factory.ChatFactory;
+import de.othr.bib48218.chat.service.IFChatService;
+import de.othr.bib48218.chat.service.IFUserService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Spy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ChatController.class)
+@Import(WebSecurityTestConfig.class)
+public class ChatControllerTest {
+    @Spy
+    HeaderSearchElementFactory headerSearchElementFactory = HeaderSearchElementFactory.getInstance();
+    @Autowired
+    private MockMvc mvc;
+    @MockBean
+    private IFUserService userService;
+    @MockBean
+    private IFChatService chatService;
+
+    private static String anyUsername() {
+        return Faker.instance().name().username();
+    }
+
+    // ToDo: chat.id must not be null
+    //@Test
+    @WithMockUser
+    void shouldRedirectToChatAfterAddingUser() throws Exception {
+        Long id = 1L;
+        Chat chat = anyChat();
+        when(chatService.getChatById(id)).thenReturn(chat);
+        mvc.perform(post("/chat/"+ id + "/add").param("username", anyUsername()))
+            .andExpect(status().is3xxRedirection())
+            .andExpect(redirectedUrl("/chat/" + id));
+    }
+
+    // ToDo: chat.id must not be null
+    //@Test
+    @WithMockUser
+    void shouldDisplayAddMemberView() throws Exception {
+        Long id = 1L;
+        Chat chat = anyChat();
+        when(chatService.getChatById(id)).thenReturn(chat);
+        mvc.perform(get("/chat/" + id + "/add"))
+            .andExpect(status().isOk());
+    }
+
+    private static Chat anyChat() {
+        return ChatFactory.newValidGroupChat();
+    }
+}
