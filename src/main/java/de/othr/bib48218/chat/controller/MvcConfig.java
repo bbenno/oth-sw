16cf40ce1341d458b8ed8b10f278d52ccf58c4b7
Benno Bielmeier <32938211+bbenno@users.noreply.github.com>
2021-01-04 20:37:59 +0100

    Extend i18n (#46)
    * Add lang.yml
    
    * Update templates regarding changed I18n keys
    
    * Change crowdin.yml to translate YML file
    
    * Add ymlMessageSourceLoader to MvcConfig
    
    * Disabled by default

--- src/main/java/de/othr/bib48218/chat/controller/MvcConfig.java
+++ src/main/java/de/othr/bib48218/chat/controller/MvcConfig.java
@@ -1,22 +1,40 @@
 package de.othr.bib48218.chat.controller;
 
 import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-import org.thymeleaf.spring5.SpringTemplateEngine;
 
 import java.util.Locale;
+import java.util.Properties;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+    //@Bean
+    public Properties yamlProperties() {
+        YamlPropertiesFactoryBean bean = new YamlPropertiesFactoryBean();
+        bean.setResources(new ClassPathResource("lang.yml"), new ClassPathResource("lang_de.yml"));
+        return bean.getObject();
+    }
+
+    //@Bean
+    public MessageSource ymlMessageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setCommonMessages(yamlProperties());
+        return messageSource;
+    }
+
     @Bean
+    @Primary
     public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("lang");
