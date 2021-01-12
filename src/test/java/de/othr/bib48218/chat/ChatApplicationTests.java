3e40aec9477b21a7f5714e9f6d34e297f0aa1873
Benno Bielmeier <github@bbenno.com>
2021-01-12 09:25:12 +0100

    Use H2 database with testing profile in tests

--- src/test/java/de/othr/bib48218/chat/ChatApplicationTests.java
+++ src/test/java/de/othr/bib48218/chat/ChatApplicationTests.java
@@ -2,8 +2,10 @@ package de.othr.bib48218.chat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("testing")
 class ChatApplicationTests {
 
     @SuppressWarnings("EmptyMethod")
--- src/test/java/de/othr/bib48218/chat/repository/MessageRepositoryIntegrationTest.java
+++ src/test/java/de/othr/bib48218/chat/repository/MessageRepositoryIntegrationTest.java
@@ -11,13 +11,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Collection;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("testing")
 public class MessageRepositoryIntegrationTest {
     @Autowired
     private TestEntityManager entityManager;
