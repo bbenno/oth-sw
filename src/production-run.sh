69832cae4a361009c0df236221da744f3f3350d7
Benno Bielmeier <github@bbenno.com>
2021-01-12 08:45:49 +0100

    Add production-run.sh

---
+++ src/production-run.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+PID_FILE=pid.chat
+
+if [ -r "${PID_FILE}" ]; then
+	echo "Found pid file"
+	pid=$(cat "${PID_FILE}")
+	echo "Prepare killing process with pid=${pid}"
+	if [ -e "/proc/${pid}" ] && [ -e "/proc/${pid}/exe" ]; then
+		kill "${pid}"
+		echo "Process killed"
+	else
+		echo "Skip killing process"
+	fi
+	rm "${PID_FILE}"
+	echo "Removed pid file"
+fi
+
+echo "Start Application"
+(
+	current_app=$(find . -type f | grep -E "chat-.*\.jar" | sort -nr | head -n1)
+	echo "Using ${current_app}"
+	java -jar "$current_app" >>logs/console.log 2>>logs/error.log &
+	echo $! >$PID_FILE &
+)
