e4be4d2653d767420c9cecabdd72d1a16a30491c
Benno Bielmeier <github@bbenno.com>
2021-01-17 11:49:06 +0100

    Ignore validation violations for message
    just return to chat

--- src/main/java/de/othr/bib48218/chat/controller/MessageController.java
+++ src/main/java/de/othr/bib48218/chat/controller/MessageController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.ConstraintViolationException;
 import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -31,9 +32,16 @@ public class MessageController {
     @RequestMapping("/new")
     private ModelAndView createMessage(@RequestParam("message") String text, @RequestParam("chat") Long chat_id, Principal principal) {
         Optional<User> author = userService.getUserByUsername(principal.getName());
-        Chat chat = chatService.getChatById(chat_id).get();
-        Message message = new Message(text, chat, author.get(), LocalDateTime.now());
-        messageService.saveMessage(message);
+        Optional<? extends Chat> chat_opt = chatService.getChatById(chat_id);
+        if (chat_opt.isEmpty() || !chatService.isUserMember(author.get(), chat_opt.get()))
+            return new ModelAndView("redirect:/", "notification", "chat not found");
+
+        Chat chat = chat_opt.get();
+        try {
+            Message message = new Message(text, chat, author.get(), LocalDateTime.now());
+            messageService.saveMessage(message);
+        } catch (ConstraintViolationException ignored) {
+        }
         return new ModelAndView("redirect:/chat/" + chat.getId());
     }
 }
