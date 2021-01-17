e5514ca421be1107787d792ba56c1920db6dd155
Benno Bielmeier <github@bbenno.com>
2021-01-17 13:00:37 +0100

    Use user/show in preference to user/show_person view

--- src/main/java/de/othr/bib48218/chat/controller/UserController.java
+++ src/main/java/de/othr/bib48218/chat/controller/UserController.java
@@ -2,27 +2,31 @@ package de.othr.bib48218.chat.controller;
 
 import de.othr.bib48218.chat.entity.Person;
 import de.othr.bib48218.chat.entity.User;
-import de.othr.bib48218.chat.service.UserService;
+import de.othr.bib48218.chat.service.IFUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.security.Principal;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
     @Autowired
-    private UserService userService;
+    private IFUserService userService;
 
     @RequestMapping("{username}")
     public ModelAndView showUser(@PathVariable String username) {
         Optional<User> found = userService.getUserByUsername(username);
         if (found.isPresent()) {
-            Person person = (Person) found.get();
-            return new ModelAndView("user/show_person", "person", person);
+            return new ModelAndView("user/show", "user", found.get());
         } else {
             return new ModelAndView("redirect:new");
         }
