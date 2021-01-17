be66f74061ac8731330a83a3ae8f34d3f33ea460
Benno Bielmeier <github@bbenno.com>
2021-01-17 18:49:51 +0100

    Remove redundant method in BotRepository

--- src/main/java/de/othr/bib48218/chat/repository/BotRepository.java
+++ src/main/java/de/othr/bib48218/chat/repository/BotRepository.java
@@ -2,9 +2,5 @@ package de.othr.bib48218.chat.repository;
 
 import de.othr.bib48218.chat.entity.Bot;
 
-import java.util.Optional;
-
 public interface BotRepository extends UserRepository<Bot> {
-    @Override
-    Optional<Bot> findById(String id);
 }
