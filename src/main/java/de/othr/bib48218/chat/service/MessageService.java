4b17ce129a2d190efb0476c62b79711083bab539
Benno Bielmeier <github@bbenno.com>
2021-01-23 10:41:21 +0100

    Update MessageService
    * mark all methods as Transactional
    * implement delete methods

--- src/main/java/de/othr/bib48218/chat/service/MessageService.java
+++ src/main/java/de/othr/bib48218/chat/service/MessageService.java
@@ -10,6 +10,7 @@ import java.util.Collection;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MessageService implements IFMessageService {
@@ -21,49 +22,61 @@ public class MessageService implements IFMessageService {
     private PersonRepository userRepository;
 
     @Override
+    @Transactional
     public Collection<Message> getAllMessagesByChat(Chat chat) {
         return repository.findByChat(chat);
     }
 
     @Override
+    @Transactional
     public Collection<Message> getMessagesByChatSince(Chat chat, LocalDateTime time) {
         return repository.findByChatAndTimestampBefore(chat, time);
     }
 
     @Override
+    @Transactional
     public Collection<Message> getAllMessagesByChatFrom(Chat chat, String username) {
         return repository.findByChatAndAuthor_Username(chat, username);
     }
 
     @Override
-    public Collection<Message> getMessagesByChatSinceFrom(Chat chat, LocalDateTime localDateTime,
-        String username) {
+    @Transactional
+    public Collection<Message> getMessagesByChatSinceFrom(
+        Chat chat,
+        LocalDateTime localDateTime,
+        String username
+    ) {
         return repository
             .findByChatAndTimestampBeforeAndAuthor_Username(chat, localDateTime, username);
     }
 
     @Override
+    @Transactional
     public Message saveMessage(Message message) {
         return repository.save(message);
     }
 
     @Override
+    @Transactional
     public void deleteMessageById(Long id) {
-
+        repository.deleteById(id);
     }
 
     @Override
+    @Transactional
     public void deleteMessage(Message message) {
-
+        repository.delete(message);
     }
 
     @Override
+    @Transactional
     public Optional<User> findUserByUsername(String username, String serviceToken) {
         // TODO: Check serviceToken
         return userRepository.findById(username).map((person) -> person);
     }
 
     @Override
+    @Transactional
     public Boolean sendMessage(Message message, String serviceToken) {
         // TODO: Check serviceToken
         try {
@@ -75,6 +88,7 @@ public class MessageService implements IFMessageService {
     }
 
     @Override
+    @Transactional
     public Collection<Message> pullMessages(String serviceToken) {
         // TODO: Check serviceToken
         User serviceUser = null;
