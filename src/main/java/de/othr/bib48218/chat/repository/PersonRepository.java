50e8e6bf525093552e5a3764534fbc852df1c443
Benno Bielmeier <github@bbenno.com>
2021-01-24 16:07:51 +0100

    Extend PersonRepository for searching

--- src/main/java/de/othr/bib48218/chat/repository/PersonRepository.java
+++ src/main/java/de/othr/bib48218/chat/repository/PersonRepository.java
@@ -1,11 +1,16 @@
 package de.othr.bib48218.chat.repository;
 
 import de.othr.bib48218.chat.entity.Person;
+import java.util.Collection;
 import java.util.List;
 
 public interface PersonRepository extends UserRepository<Person> {
 
     List<Person> findByFirstNameOrderByFirstName(String firstName);
 
+    Collection<Person> findByFirstNameContains(String firstNameFragment);
+
     List<Person> findByLastNameOrderByLastName(String lastName);
+
+    Collection<Person> findByLastNameContains(String lastNameFragment);
 }
