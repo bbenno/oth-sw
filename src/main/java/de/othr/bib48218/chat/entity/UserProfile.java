c92e0ca5d049482af7ea76c020fea6e7402a609e
Benno Bielmeier <github@bbenno.com>
2021-01-24 21:12:19 +0100

    Remove user attribute from UserProfile

--- src/main/java/de/othr/bib48218/chat/entity/UserProfile.java
+++ src/main/java/de/othr/bib48218/chat/entity/UserProfile.java
@@ -1,7 +1,6 @@
 package de.othr.bib48218.chat.entity;
 
 import javax.persistence.Entity;
-import javax.persistence.OneToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.lang.NonNull;
@@ -14,9 +13,6 @@ public class UserProfile extends Profile {
     private String bio;
 
     private String country;
-
-    @OneToOne(mappedBy = "profile")
-    private User user;
 
     public UserProfile(@NonNull String name) {
         super(name);
