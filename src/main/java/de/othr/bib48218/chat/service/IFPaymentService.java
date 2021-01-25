a7be4eeab78ab8f07722307c7135c22b3a28d098
Benno Bielmeier <github@bbenno.com>
2021-01-25 23:33:32 +0100

    Add IFPaymentService as extension of TransferServiceExternalIF

---
+++ src/main/java/de/othr/bib48218/chat/service/IFPaymentService.java
@@ -0,0 +1,11 @@
+package de.othr.bib48218.chat.service;
+
+import com.othr.swvigopay.entity.TransferDTO;
+import com.othr.swvigopay.exceptions.TransferServiceExternalException;
+import com.othr.swvigopay.service.TransferServiceExternalIF;
+
+public interface IFPaymentService extends TransferServiceExternalIF {
+
+    TransferDTO transfer(TransferDTO transferDTO) throws TransferServiceExternalException;
+
+}
