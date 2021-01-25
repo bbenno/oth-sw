f1ad189345eba1d161393b39017fb627d8b9b14a
Benno Bielmeier <github@bbenno.com>
2021-01-25 12:58:48 +0100

    Add PaymentService

---
+++ src/main/java/de/othr/bib48218/chat/service/PaymentService.java
@@ -0,0 +1,29 @@
+package de.othr.bib48218.chat.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.WebApplicationContext;
+
+@Service
+@Scope(WebApplicationContext.SCOPE_REQUEST)
+public class PaymentService {
+
+    @Value("${partner.service.payment.port}")
+    private int port;
+
+    @Value("${partner.service.payment.api-path}")
+    private String apiPath;
+
+    @Autowired
+    private RestTemplate restServiceClient;
+
+    public void test() {
+    }
+
+    protected String getAPIUrl() {
+        return "http://localhost/" + port + "/" + apiPath;
+    }
+}
