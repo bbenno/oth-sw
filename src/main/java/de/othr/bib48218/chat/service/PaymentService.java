e07208f354cdaa68cbf84f7438451cb815af303e
Benno Bielmeier <github@bbenno.com>
2021-01-25 23:05:30 +0100

    Let PaymentService implement TransferServiceExternalIF

--- src/main/java/de/othr/bib48218/chat/service/PaymentService.java
+++ src/main/java/de/othr/bib48218/chat/service/PaymentService.java
@@ -1,12 +1,16 @@
 package de.othr.bib48218.chat.service;
 
+import com.othr.swvigopay.entity.TransferDTO;
+import com.othr.swvigopay.exceptions.TransferServiceExternalException;
+import com.othr.swvigopay.service.TransferServiceExternalIF;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public class PaymentService {
+public class PaymentService implements TransferServiceExternalIF {
 
     @Value("${partner.service.payment.port}")
     private int port;
@@ -17,10 +21,15 @@ public class PaymentService {
     @Autowired
     private RestTemplate restServiceClient;
 
-    public void test() {
-    }
-
     protected String getAPIUrl() {
         return "http://localhost/" + port + "/" + apiPath;
     }
+
+    @Override
+    public ResponseEntity<TransferDTO> requestTransferExternal(
+        TransferDTO transferDTO
+    ) throws TransferServiceExternalException {
+        return null;
+    }
+
 }
