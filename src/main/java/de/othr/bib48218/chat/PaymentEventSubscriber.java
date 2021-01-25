e23eefa73bc3b0dad25baae0ec695d9723343659
Benno Bielmeier <github@bbenno.com>
2021-01-25 13:17:51 +0100

    Add PaymentEventSubscriber

---
+++ src/main/java/de/othr/bib48218/chat/PaymentEventSubscriber.java
@@ -0,0 +1,17 @@
+package de.othr.bib48218.chat;
+
+import de.othr.bib48218.chat.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentEventSubscriber implements ApplicationListener<PartnerServiceEvent> {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @Override
+    public void onApplicationEvent(PartnerServiceEvent event) {
+    }
+}
