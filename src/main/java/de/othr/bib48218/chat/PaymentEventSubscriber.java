f1b426c050ce2ea626c21dac11122a8d5290756b
Benno Bielmeier <github@bbenno.com>
2021-01-25 23:07:11 +0100

    Use TransferServiceExternalIF for handling of PartnerServiceEvents

--- src/main/java/de/othr/bib48218/chat/PaymentEventSubscriber.java
+++ src/main/java/de/othr/bib48218/chat/PaymentEventSubscriber.java
@@ -1,7 +1,14 @@
 package de.othr.bib48218.chat;
 
+import com.othr.swvigopay.entity.TransferDTO;
+import com.othr.swvigopay.exceptions.TransferServiceExternalException;
+import com.othr.swvigopay.service.TransferServiceExternalIF;
+import de.othr.bib48218.chat.entity.Message;
+import de.othr.bib48218.chat.entity.Person;
 import de.othr.bib48218.chat.entity.ServiceType;
-import de.othr.bib48218.chat.service.PaymentService;
+import java.math.BigDecimal;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
@@ -9,13 +16,31 @@ import org.springframework.stereotype.Component;
 @Component
 public class PaymentEventSubscriber implements ApplicationListener<PartnerServiceEvent> {
 
+    private static final Pattern transferMessage = Pattern.compile(
+        "^/transfer (?<amount>[0-9]+(\\.[0-9]*)?) (?<email>[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6})\\s*(?<description>.*)$",
+        Pattern.CASE_INSENSITIVE);
+
     @Autowired
-    private PaymentService paymentService;
+    private TransferServiceExternalIF paymentService;
 
     @Override
     public void onApplicationEvent(PartnerServiceEvent event) {
         if (event.getServiceType() == ServiceType.PAYMENT) {
-            paymentService.test();
+            Message message = event.getSource();
+            Matcher matcher = transferMessage.matcher(message.getText());
+            if (matcher.matches()) {
+                String payerEmail = ((Person) message.getAuthor()).getEmail();
+                String receiverEmail = matcher.group("email");
+                BigDecimal amount = BigDecimal.valueOf(Long.parseLong(matcher.group("amount")));
+                String description = matcher.group("description");
+
+                TransferDTO dto = new TransferDTO(payerEmail, receiverEmail, amount, description);
+
+                try {
+                    paymentService.requestTransferExternal(dto);
+                } catch (TransferServiceExternalException e) {
+                }
+            }
         }
     }
 }
