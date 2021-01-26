7dfca09f732c5b7df5946372602d5b7f0e3d216d
Benno Bielmeier <github@bbenno.com>
2021-01-26 08:52:03 +0100

    Fix MessageRestControllerV1: add missing annotation

--- src/main/java/de/othr/bib48218/chat/rest/MessageRestControllerV1.java
+++ src/main/java/de/othr/bib48218/chat/rest/MessageRestControllerV1.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +30,7 @@ public class MessageRestControllerV1 implements IFMessageRestControllerV1 {
 
     @Override
     @PostMapping()
-    public ResponseEntity<Message> postMessage(Message message) {
+    public ResponseEntity<Message> postMessage(@RequestBody Message message) {
         return ResponseEntity.of(Optional.ofNullable(messageService.saveMessage(message)));
     }
 
