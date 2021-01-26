175c704ee8e6105bd2bed28e4223f610186def8b
Benno Bielmeier <github@bbenno.com>
2021-01-26 10:14:48 +0100

    Update import.sql according to schema

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,6 @@
+INSERT INTO user_profile(id, name, bio)
+VALUES (1, "Vigo Pay", "Bot to request mony from other persons.");
 
-INSERT INTO user_profile(id, name) VALUES(1, "Vigo Pay");
-INSERT INTO bot(username, password, profile_id) VALUES("payment_service", "", 1);
+INSERT INTO bot(username, password, profile_id, scope, account_non_expired, account_non_locked,
+                credentials_non_expired, enabled)
+VALUES ("payment_service", "", 1, 4, true, true, false, true);
