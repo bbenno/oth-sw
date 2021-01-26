6bf098e6c69f00e9102186e5601a960cc4ed7120
Benno Bielmeier <github@bbenno.com>
2021-01-26 19:23:30 +0100

    Add JavaDoc to ApplicationControllerAdvice

--- src/main/java/de/othr/bib48218/chat/controller/ApplicationControllerAdvice.java
+++ src/main/java/de/othr/bib48218/chat/controller/ApplicationControllerAdvice.java
@@ -4,15 +4,26 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+/**
+ * Advice for controller.
+ */
 @ControllerAdvice
 public class ApplicationControllerAdvice {
 
+    /**
+     * The name of this application.
+     */
     @Value("${spring.application.name}")
     private String applicationName;
 
+    /**
+     * Gets the name of this application.
+     *
+     * @return the name of this application
+     */
     @ModelAttribute("applicationName")
     public String getApplicationName() {
         return applicationName;
     }
-    
+
 }
