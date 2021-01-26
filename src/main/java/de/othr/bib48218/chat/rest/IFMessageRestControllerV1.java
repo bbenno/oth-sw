a59e4d13281c9a7d50921bc7064599d00bba5a28
Benno Bielmeier <github@bbenno.com>
2021-01-26 13:04:53 +0100

    Add JavaDoc to REST controller interfaces

--- src/main/java/de/othr/bib48218/chat/rest/IFMessageRestControllerV1.java
+++ src/main/java/de/othr/bib48218/chat/rest/IFMessageRestControllerV1.java
@@ -4,32 +4,35 @@ import de.othr.bib48218.chat.entity.Message;
 import java.util.Collection;
 import org.springframework.http.ResponseEntity;
 
+/**
+ * A REST controller (v1 of web API) for {@link de.othr.bib48218.chat.entity.Message}s.
+ */
 public interface IFMessageRestControllerV1 {
 
-    /* CREATE  ************************************************************************************/
+  /* CREATE  ************************************************************************************/
 
-    /**
-     * Saves given message.
-     *
-     * @param message the message to save
-     * @return saved message
-     */
-    ResponseEntity<Message> postMessage(Message message);
+  /**
+   * Saves a certain message.
+   *
+   * @param message the message to save
+   * @return the saved message
+   */
+  ResponseEntity<Message> postMessage(Message message);
 
-    /* READ  **************************************************************************************/
+  /* READ  **************************************************************************************/
 
-    /**
-     * Returns messages
-     *
-     * @param chatId   the id of the chat whose messages to receive
-     * @param dateTime the optional date time since when messages to receive
-     * @param username the username of the author
-     * @return messages matching parameter constraint
-     */
-    ResponseEntity<Collection<Message>> getMessages(Long chatId, String dateTime, String username);
+  /**
+   * Gets all messages in a certain chat since a certain point in time and of a certain author.
+   *
+   * @param chatId   the number identifying the chat
+   * @param dateTime the date time
+   * @param username the string identifying the user
+   * @return the collection containing the messages
+   */
+  ResponseEntity<Collection<Message>> getMessages(Long chatId, String dateTime, String username);
 
-    /* UPDATE  ************************************************************************************/
+  /* UPDATE  ************************************************************************************/
 
-    /* DELETE  ************************************************************************************/
+  /* DELETE  ************************************************************************************/
 
 }
