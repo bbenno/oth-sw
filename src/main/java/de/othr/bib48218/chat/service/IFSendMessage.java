ddd1567c37961d30c1c6f3eb82c875e0691a3e84
Benno Bielmeier <github@bbenno.com>
2021-01-26 22:02:47 +0100

    Update IFSendMessage interface in agreement with partner

--- src/main/java/de/othr/bib48218/chat/service/IFSendMessage.java
+++ src/main/java/de/othr/bib48218/chat/service/IFSendMessage.java
@@ -1,9 +1,10 @@
 package de.othr.bib48218.chat.service;
 
+import de.othr.bib48218.chat.entity.Chat;
 import de.othr.bib48218.chat.entity.Message;
 import de.othr.bib48218.chat.entity.User;
+import java.time.LocalDateTime;
 import java.util.Collection;
-import java.util.Optional;
 
 @SuppressWarnings("unused")
 public interface IFSendMessage {
@@ -11,24 +12,31 @@ public interface IFSendMessage {
     /**
      * Gets the user with a certain username
      *
-     * @param username the string identifying user
+     * @param username the string identifying a user
      * @return the user
      */
-    Optional<User> findUserByUsername(String username);
+    User getUser(String username);
+
+    /**
+     * Gets the chat of the user identified by its username with the service bot.
+     *
+     * @param username the string identifying a user
+     * @return the chat
+     */
+    Chat getChatWithUserByUsername(String username);
 
     /**
      * Saves a certain message.
      *
      * @param message the message
-     * @return <code>true</code> if sending was successful; otherwise <code>false</code>
      */
-    Boolean sendMessage(Message message);
+    void sendMessage(Message message);
 
     /**
      * Gets all messages.
      *
      * @return the collection containing all messages
      */
-    Collection<Message> pullMessages();
+    Collection<Message> pullMessages(Chat chat, LocalDateTime dateTime);
 
 }
