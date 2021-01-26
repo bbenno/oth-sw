3f62b5a962db35da6b465b1691be90f3b36de827
Benno Bielmeier <github@bbenno.com>
2021-01-26 10:15:19 +0100

    Fix

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,3 @@
-INSERT INTO user_profile(id, name, bio)
-VALUES (1, "Vigo Pay", "Bot to request mony from other persons.");
+INSERT INTO user_profile(id, `name`, bio) VALUES (1, 'Vigo Pay', 'Bot to request money from other persons.');
+INSERT INTO bot(username, account_non_expired, account_non_locked, credentials_non_expired, enabled, password, `scope`, profile_id) VALUES ('payment_service', TRUE, TRUE, FALSE, TRUE, '_', 4, 1);
 
-INSERT INTO bot(username, password, profile_id, scope, account_non_expired, account_non_locked,
-                credentials_non_expired, enabled)
-VALUES ("payment_service", "", 1, 4, true, true, false, true);
