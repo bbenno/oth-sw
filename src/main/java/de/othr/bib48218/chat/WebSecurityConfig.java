c1acee8bbd33d0993e7d46a0568b744a3707bebd
Benno Bielmeier <github@bbenno.com>
2021-01-28 16:10:09 +0100

    Add Logo to login

--- src/main/java/de/othr/bib48218/chat/WebSecurityConfig.java
+++ src/main/java/de/othr/bib48218/chat/WebSecurityConfig.java
@@ -20,7 +20,7 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String[] ALLOW_ACCESS_WITHOUT_AUTHENTICATION = {"/css/**", "/img/**",
         "/fonts/**", "/js/**", "/login", "/register", "/favicon*", "/apple-*.png", "/android-*.png",
-        "/site.webmanifest", "/webjars/**", "/webapi/**", "/api/callback/**"};
+        "/site.webmanifest", "/webjars/**", "/webapi/**", "/api/callback/**", "/logo*"};
 
     @Autowired
     private UserDetailsService userSecurityService;
